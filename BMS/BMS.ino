6b71285d9b653b2782f12644ba448530d672e183
maskac <maska@cgcs.cz>
2016-11-21 13:23:02 +0100

    corrected comments

--- BMS/BMS.ino
+++ BMS/BMS.ino
@@ -1,17 +1,20 @@
 #include "vcc.h"
 #include <SoftwareSerial.h>
-SoftwareSerial Serial(3,4); 
+SoftwareSerial SW_Serial(3,4); 
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);  
+  //enable software serial on 9600 for debuging
+  SW_Serial.begin(9600);  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // temporary variable for reading
   float x;
+  // read Vcc voltage with quadratic correction
   x=readVccpow(-0.045,1.1882,-0.1432,50);
-  Serial.println(x);
+  // print readed voltage in V
+  SW_Serial.println(x);
+  // pause 500ms
   delay(500);
   
 }
